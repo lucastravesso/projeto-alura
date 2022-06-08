f042dddc5befd88fbffdae978c18eb71e1e98b28
Lucas Escobar Faria Travesso <lucas.travesso@muralis.com.br>
2022-06-08 17:43:15 -0300

    atualização de bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const controle = document.querySelectorAll("[data-controle]")
 const estatisticas = document.querySelectorAll("[data-estatistica]")
+
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -34,16 +35,15 @@ const pecas = {
     }
 }
 
-console.log(estatisticas)
-
 controle.forEach((e) => {
     e.addEventListener("click", (element) => {
         manipulaDados(element.target.dataset.controle, element.target.parentNode)
-        atualizaEstatisticas(element.target.dataset.peca)
+        const check = element.target.parentNode.querySelector("[data-counter]")
+        if (check.value == "00") {return } else {
+            atualizaEstatisticas(element.target.dataset.controle, element.target.dataset.peca)
+        }
     })
 })
-
-
 
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-counter]")
@@ -53,15 +53,19 @@ function manipulaDados(operacao, controle) {
             peca.value = parseInt(peca.value) - 1
         } else {
             peca.value = "00"
-            alert("Valor nao pode ser menor que 0")
         }
     } else {
         peca.value = parseInt(peca.value) + 1
     }
 }
 
-function atualizaEstatisticas(peca) {
+function atualizaEstatisticas(operacao, peca) {
+
     estatisticas.forEach((e) => {
-        e.textContent = parseInt(e.textContent) + pecas[peca][e.dataset.estatistica]
+        if (operacao === "+") {
+            e.textContent = parseInt(e.textContent) + pecas[peca][e.dataset.estatistica]
+        } else if (operacao === "-") {
+            e.textContent = parseInt(e.textContent) - pecas[peca][e.dataset.estatistica]
+        }
     })
 }
